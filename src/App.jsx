6dda00b307361e63759d151ac951f0565d4e96b6
MykhailoVobolis <mvobolis@gmail.com>
2024-04-07 16:07:36 +0300

    Task-03 ready

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,8 @@ import data from './data/stats.json';
 
 import article from 'data/article.json';
 
+import forbes from 'data/forbes.json';
+
 export const App = () => {
   return (
     <Section>
@@ -30,7 +32,7 @@ export const App = () => {
         <Heading title="Task 2 Statistics" top bottom />
         <Statistics title="Main Statistics" data={data} />
         <Heading title="Task 3 Forbes list" top bottom />
-        <ForbesList />
+        <ForbesList forbes={forbes} />
         <Heading title="Task 4 Crypto history" top bottom />
         <CryptoHistory />
       </Container>
--- src/components/ForbesList/ForbesList.jsx
+++ src/components/ForbesList/ForbesList.jsx
@@ -1,3 +1,29 @@
-export const ForbesList = () => {
-  return <h2>ForbesList</h2>;
+import style from './ForbesList.module.css';
+import { ForbesListItem } from 'components/ForbesListItem/ForbesListItem';
+
+export const ForbesList = ({ forbes }) => {
+  return (
+    <div className={style.board}>
+      <div className={style.header}>
+        <h2 className={style.title}>
+          <span className={style.titleTop}>Forbes</span>
+          <span className={style.titleBottom}>Leader board</span>
+        </h2>
+      </div>
+
+      <ul className={style.list}>
+        {forbes.map(({ id, name, capital, avatar, isIncrease }) => {
+          return (
+            <ForbesListItem
+              key={id}
+              name={name}
+              capital={capital}
+              avatar={avatar}
+              isIncrease={isIncrease}
+            />
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
